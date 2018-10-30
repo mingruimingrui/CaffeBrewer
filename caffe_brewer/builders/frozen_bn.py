d2540f46cf7bdfd96df9f7388b810184486fdfc0
mingruimingrui <mingrui.wang@shopee.com>
2018-10-30 21:40:38 +0800

    added builder folder for storing common builder functions

---
+++ caffe_brewer/builders/frozen_bn.py
@@ -0,0 +1,31 @@
+from caffe2.python.modeling import initializers
+from caffe2.python.modeling.parameter_info import ParameterTags
+
+
+def add_frozen_bn(model, blob_in, blob_out, dim, inplace=False):
+    """
+    Affine transformation to replace BN in networks where BN cannot be used.
+    (eg. when minibatch size is too small)
+
+    The operation can be done inplace to save memory.
+    """
+    prefix = blob_out
+
+    weight = model.create_param(
+        param_name=prefix + '_w',
+        initializer=initializers.Initializer('ConstantFill', value=1.0),
+        tags=ParameterTags.WEIGHT,
+        shape=[dim, ]
+    )
+
+    bias = model.create_param(
+        param_name=prefix + '_b',
+        initializer=initializers.Initializer('ConstantFill', value=0.0),
+        tags=ParameterTags.BIAS,
+        shape=[dim, ]
+    )
+
+    if inplace:
+        return model.net.AffineChannel([blob_in, weight, bias], blob_in)
+    else:
+        return model.net.AffineChannel([blob_in, weight, bias], blob_out)
