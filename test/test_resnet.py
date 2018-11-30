7a4198e27a750e3aee81783ce41d556da71fb724
mingruimingrui <mingruimingrui@hotmail.com>
2018-11-30 18:16:28 +0800

    added basic test for resnet

--- test/test_resnet.py
+++ test/test_resnet.py
@@ -6,14 +6,24 @@ test_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir = os.path.abspath(os.path.join(test_dir, os.pardir))
 sys.path.append(root_dir)
 
+import pytest
+
 from PIL import Image
 import numpy as np
+
+import torch
+import torchvision
 
 from caffe2.python import workspace, core, model_helper
 from caffe_brewer.resnet import add_resnet_ops, load_pretrained_weights
 
 sample_image_path = os.path.join(test_dir, 'data', 'mug.jpg')
 assert os.path.isfile(sample_image_path)
+
+
+def calc_mean_diff(A, B):
+    diff = np.array(A) - np.array(B)
+    return np.mean(np.abs(diff))
 
 
 def preproc_image(image):
@@ -44,7 +54,7 @@ def add_image_blob(image_blob_name='image'):
         workspace.CreateBlob(scoped_image_blob, device_option=device_opt)
         workspace.FeedBlob(scoped_image_blob, image, device_option=device_opt)
 
-    return image_blob_name
+    return image_blob_name, image
 
 
 def make_default_model_helper(model_name='resnet'):
@@ -62,15 +72,16 @@ def test_resnet_build():
     """
     Test if add_resnet_ops can be built
     """
+    pass
     scope_name = 'test'
-    model_name = 'resnet'
+    model_name = 'test_resnet_build'
     image_blob_name = 'image'
 
     workspace.ResetWorkspace()
     model = make_default_model_helper(model_name)
 
     with core.NameScope(scope_name):
-        blob_in = add_image_blob(image_blob_name)
+        blob_in, _ = add_image_blob(image_blob_name)
         blob_out, resnet_config = add_resnet_ops(model, blob_in)
 
     workspace.RunNetOnce(model.param_init_net)
@@ -80,3 +91,46 @@ def test_resnet_build():
         load_pretrained_weights(resnet_config.TYPE)
 
     workspace.RunNet(model_name)
+
+
+@pytest.mark.parametrize('resnet_type', [
+    'resnet18',
+    'resnet50'
+])
+def test_resnet_pretrained(resnet_type):
+    """
+    Test that weights are transferred correctly
+    """
+    scope_name = 'test'
+    model_name = 'test_resnet_pretrained_' + resnet_type
+    image_blob_name = 'image'
+
+    # Generate Caffe output
+    workspace.ResetWorkspace()
+    model = make_default_model_helper(model_name)
+
+    with core.NameScope(scope_name):
+        blob_in, image = add_image_blob(image_blob_name)
+        blob_out, resnet_config = \
+            add_resnet_ops(model, blob_in, TYPE=resnet_type)
+
+    workspace.RunNetOnce(model.param_init_net)
+    workspace.CreateNet(model.net, overwrite=True)
+
+    with core.NameScope(scope_name):
+        load_pretrained_weights(resnet_config.TYPE)
+
+    workspace.RunNet(model_name)
+    caffe_output = workspace.FetchBlob('{}/softmax'.format(scope_name))
+
+    # Generate Torch output
+    torch_model_fn = getattr(torchvision.models, resnet_type)
+    torch_model = torch_model_fn(pretrained=True).eval()
+    torch_output = torch_model(torch.Tensor(image))
+    torch_output = torch.softmax(torch_output, 1)
+    torch_output = torch_output.cpu().data.numpy()
+
+    # Ensure that the difference is not too much
+    mean_diff = calc_mean_diff(caffe_output, torch_output)
+    assert mean_diff < 1e-7, \
+        'Difference between caffe_output and torch_output too large {}'.format()
